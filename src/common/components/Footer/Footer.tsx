f0dc0bfc91c609a4345ea0e6702268e1ef1e17fa
annatulkina <an.tyulkina@mail.ru>
2026-05-21 17:05:26 +0300

    footer

--- src/common/components/Footer/Footer.tsx
+++ src/common/components/Footer/Footer.tsx
@@ -2,6 +2,8 @@ import s from './Footer.module.css'
 
 export const Footer = ()=>{
     return (
-        <div className={s.footer}>footer</div>
+        <div className={s.footer}>
+            <small>© 2025 Kinopoisk Demo · Data courtesy of TMDB.</small>
+        </div>
     )
 }
