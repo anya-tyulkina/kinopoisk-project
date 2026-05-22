c9bbb487fc6e9075466b680c26da5cbc1e788c68
annatulkina <an.tyulkina@mail.ru>
2026-05-22 15:37:15 +0300

    api

--- api/tmdb-token.js
+++ api/tmdb-token.js
@@ -1,5 +1,21 @@
 module.exports = async function handler(req, res) {
-    res.status(200).json({
-        hello: 'world'
-    });
+    try {
+        const response = await fetch(
+            'https://api.themoviedb.org/3/authentication/token/new',
+            {
+                headers: {
+                    accept: 'application/json',
+                    Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`
+                }
+            }
+        );
+
+        const data = await response.json();
+
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({
+            error: error.message
+        });
+    }
 };
